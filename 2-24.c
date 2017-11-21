2bb294d88f0fa688f76296f1957c033dc40e177e
Thawatchai09 <31584456+Thawatchai09@users.noreply.github.com>
2017-11-21 23:45:10 +0700

    Create 2-24.c

---
+++ 2-24.c
@@ -0,0 +1,7 @@
+# include < stdio.h >
+int  main ()
+{
+	printf ( "อายุ = % d , GPA = % .2f \ n " , 30 , 4.00 );
+	printf ( "การเขียนโปรแกรม: % .1f \ n Mechanice: % .1f " , 4.0 , 3.5 );
+	return  0 ;
+}
