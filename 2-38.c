d875bd2199ccf57119578c32c8d1d5a726715b3e
Thawatchai09 <31584456+Thawatchai09@users.noreply.github.com>
2017-11-21 23:46:47 +0700

    Create 2-38.c

---
+++ 2-38.c
@@ -0,0 +1,15 @@
+# include < stdio.h >
+int  main ()
+{
+	printf ( " % s \ n " , " 58224090122 " );
+	printf ( " % s \ n " , "อนุชิตชานชิ" );
+	printf ( " % s \ n " , " 16/08/1987 " );
+	printf ( " \ n " );
+	printf ( " \ n " );
+	printf ( " \ n " );
+	printf ( " \ n " );
+	printf ( " % 120s \ n " , " http://www.kru.ac.th " );
+	printf ( " \ n " );
+	printf ( " % 57s% s " , " " , " BYE BYE " );
+	return  0 ;
+}
