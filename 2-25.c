2f5f1b975846a110ac2951e58363927c426e594b
Thawatchai09 <31584456+Thawatchai09@users.noreply.github.com>
2017-11-21 23:46:15 +0700

    Create 2-25.c

---
+++ 2-25.c
@@ -0,0 +1,8 @@
+# include < stdio.h >
+int  main ()
+{
+	printf ( "หัวเรื่อง: % s ( % d ) \ n " , " Programming " , 2552 );
+	printf ( "จุด: % d \ nระดับ: % c " , 99 , ' A ' );
+	printf ( " 19: % x 31: % x " , 19 , 31 );
+	return  0 ;
+}
