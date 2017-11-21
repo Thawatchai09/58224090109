bc71cb7192c1885ce0f90d27dbebe22d967dfd51
Thawatchai09 <31584456+Thawatchai09@users.noreply.github.com>
2017-11-21 23:44:40 +0700

    Create 2-21.c

---
+++ 2-21.c
@@ -0,0 +1,11 @@
+# include < stdio.h >
+int  main ()
+{
+	printf ( "ฉันชื่อ: % .2s \ n " , " Kmitl " );
+	printf ( "จุดของฉัน: % d \ n " , 10 + 40 + 49 );
+	printf ( "เกรด: % c \ n " , " A " );
+	printf ( " GPA: % .2f \ n " , 3.99 );
+	printf ( " 78 = % c \ n " , 78 );
+	printf ( " % d - % d - % d " , 16 , 8 , 30 ); // 16-8-30
+	return  0 ;
+}
